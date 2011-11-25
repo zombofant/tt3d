491200f417691a8be88799ef9b94ddc59c8fcaf5
Jonas Wielicki <j.wielicki@sotecware.net>
2011-11-25 02:24:47 +0100

    * Initial matrix code

---
+++ modules/geometry/Matrices.hpp
@@ -0,0 +1,35 @@
+#ifndef _TT3D_GEOMETRY_MATRICES_H
+#define _TT3D_GEOMETRY_MATRICES_H
+
+#include "Vectors.hpp"
+
+namespace tt3d {
+namespace Geometry {
+
+struct Matrix3 {
+    double coeff[9];
+    
+    static Matrix3 Rotation(const Vector3 axis, const VectorFloat angle);
+};
+
+struct Matrix4 {
+    double coeff[16];
+    
+    static Matrix4 Rotation(const Vector3 axis, const VectorFloat angle);
+    static Matrix4 Rotation(const Matrix3 m3);
+    static Matrix4 Translation(const Vector3 by);
+    static Matrix4 Scale(const Vector3 by);
+};
+
+Vector3 operator* (Matrix3 a, Vector3 b);
+Matrix3 operator* (Matrix3 a, Matrix3 b);
+Matrix3 operator* (Matrix3 a, VectorFloat b);
+
+Vector4 operator* (Matrix4 a, Vector4 b);
+Matrix4 operator* (Matrix4 a, Matrix4 b);
+Matrix4 operator* (Matrix4 a, VectorFloat b);
+    
+}
+}
+
+#endif
