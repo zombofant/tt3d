10b209410f61bd5dba3e7bddbb31af160ef57ffb
Jonas Wielicki <j.wielicki@sotecware.net>
2011-11-26 13:33:53 +0100

    * Implemented GL baseclass with virtual bind/unbind methods.

---
+++ modules/gl/Base.cpp
@@ -0,0 +1,34 @@
+#include <exception>
+#include <GL/glew.h>
+#include "Base.hpp"
+
+namespace tt3d {
+namespace GL {
+
+/* tt3d::GL::Error */
+
+Error::Error(const std::string aMessage):
+    message("OpenGL error: " + aMessage)
+{
+    
+}
+
+/* tt3d::GL::Class */
+
+Class::Class():
+    id(0)
+{
+    
+}
+
+/* free functions */
+
+void raiseLastGLError() {
+    const GLenum error = glGetError();
+    if (error != GL_NO_ERROR) {
+        throw Error((const char *)gluErrorString(error));
+    }
+}
+
+}
+}
--- modules/gl/Base.hpp
+++ modules/gl/Base.hpp
@@ -1,9 +1,36 @@
 #include <exception>
+#include <GL/glew.h>
+#include <string>
 
 namespace tt3d {
 namespace GL {
+    
+class Error: public std::exception {
+    public:
+        Error(const std::string aMessage);
+        ~Error() throw() {};
+    private:
+        const std::string message;
+    public:
+        const char *what() {
+            return message.c_str();
+        }
+};
 
+class Struct {
+    public:
+        virtual void bind() = 0;
+        virtual void unbind() = 0;
+};
 
+class Class: public Struct {
+    public:
+        Class();
+    protected:
+        GLuint id;
+};
+
+void raiseLastGLError();
 
 }
 }
